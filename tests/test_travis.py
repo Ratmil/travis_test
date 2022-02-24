21a96bb8a4d4098be6bc7036cfe4dc185da58ab5
Ratmil Torres <ratmil@gmail.com>
2022-02-24 16:54:58 -0300

    Try a failing test

--- tests/test_travis.py
+++ tests/test_travis.py
@@ -8,5 +8,5 @@ _logger = logging.getLogger(__name__)
 @openerp.tests.common.post_install(True)
 class TestTravisTest(openerp.tests.common.TransactionCase):
     def test_1(self):
-        self.assertTrue(True)
+        self.assertTrue(False)
        
